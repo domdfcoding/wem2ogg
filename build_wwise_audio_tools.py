b9f9b52eda8276ad8c9d9fd847f0363a9867cf92
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-11-25 16:31:34 +0000

    Force cmake

--- build_wwise_audio_tools.py
+++ build_wwise_audio_tools.py
@@ -4,5 +4,5 @@ import os
 import subprocess
 
 os.chdir("wwise-audio-tools")
-subprocess.run(["cmake", "-B", "build"], check=False)
+subprocess.run(["cmake", "-B", "build", "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"], check=False)
 subprocess.run(["cmake", "--build", "build"], check=False)
