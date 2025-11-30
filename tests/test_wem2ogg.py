6ebd061782114c63a46d93a2f1ceadf698e8ae55
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-11-30 17:49:44 +0000

    Add test for error if WEM data is mangled.

--- tests/test_wem2ogg.py
+++ tests/test_wem2ogg.py
@@ -1,4 +1,5 @@
 # 3rd party
+import pytest
 from domdf_python_tools.paths import PathPlus
 
 # this package
@@ -14,3 +15,8 @@ def test_conversion():
 	ogg_data = wem_to_ogg(wem_data)
 	assert len(ogg_data) == 1143628
 	assert ogg_data == ogg_file.read_bytes()
+
+
+def test_error():
+	with pytest.raises(ValueError, match="^Error converting wem to ogg$"):
+		wem_to_ogg(b"\x01\x02\x03\x04\x05\x06\x07\x08\x09")
