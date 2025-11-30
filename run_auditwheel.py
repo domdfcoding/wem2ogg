132123897614328eb754fdd0269f2aa71052f5d8
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-11-30 09:51:31 +0000

    Fix run_auditwheel script

--- run_auditwheel.py
+++ run_auditwheel.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
 
 # stdlib
-# import os
+import os
 import subprocess
 from pathlib import Path
 
 files = [p for p in Path("dist").glob("*.whl") if "manylinux" not in p.name]
 
-# env = os.environ.copy()
+env = os.environ.copy()
 # env["LD_LIBRARY_PATH"] = ':'.join([
 # 		os.path.join(os.getcwd(), "wwise-audio-tools/build/lib"),
 # 		os.getenv("LD_LIBRARY_PATH", ''),
