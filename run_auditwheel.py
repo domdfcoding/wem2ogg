033a9318920027ad73140874037b8838cc624066
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-11-30 11:36:46 +0000

    Compile wwise-audio-tools/libvorbis/libogg into the Python extension module. (#3)
    Also enables using system libvorbis/libogg if available.

--- run_auditwheel.py
+++ run_auditwheel.py
@@ -8,11 +8,7 @@ from pathlib import Path
 files = [p for p in Path("dist").glob("*.whl") if "manylinux" not in p.name]
 
 env = os.environ.copy()
-env["LD_LIBRARY_PATH"] = ':'.join([
-		os.path.join(os.getcwd(), "wwise-audio-tools/build/lib"),
-		os.getenv("LD_LIBRARY_PATH", ''),
-		])
-print(subprocess.check_output(["auditwheel", "repair", *files, "-w", "dist"], env=env))
+subprocess.check_output(["auditwheel", "repair", *files, "-w", "dist"], env=env)
 
 for file in files:
 	file.unlink()
