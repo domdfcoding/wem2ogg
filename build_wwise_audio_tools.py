fa41d6ebead11702b689ae294090ff1ad3b2caaa
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-11-30 17:51:28 +0000

    Remove old wwise-audio-tools build script.

--- build_wwise_audio_tools.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-# stdlib
-import os
-import subprocess
-
-os.chdir("wwise-audio-tools")
-subprocess.run(["cmake", "-B", "build", "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"], check=False)
-subprocess.run(["cmake", "--build", "build"], check=False)
