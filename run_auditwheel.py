e253834153cdf41caee0b7c952a73b1339cec296
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-11-30 14:24:07 +0000

    Only install patchelf on Linux and run delvewheel if invoking tox -e build from Windows

--- run_auditwheel.py
+++ run_auditwheel.py
@@ -2,13 +2,22 @@
 
 # stdlib
 import os
+import platform
 import subprocess
 from pathlib import Path
 
 files = [p for p in Path("dist").glob("*.whl") if "manylinux" not in p.name]
 
 env = os.environ.copy()
-subprocess.check_output(["auditwheel", "repair", *files, "-w", "dist"], env=env)
+
+if platform.system() == "Linux":
+	command = "auditwheel"
+elif platform.system() == "Windows":
+	command = "delvewheel"
+else:
+	sys.exit()
+
+subprocess.check_output([command, "repair", *files, "-w", "dist"], env=env)
 
 for file in files:
 	file.unlink()
